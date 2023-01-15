740258c1fc3d39ee96baea941918e85635440795
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2023-01-15 18:44:05 +0200

    Add files via upload

---
+++ stackoverflowsearcher/script.js
@@ -0,0 +1,6 @@
+
+function searchst()
+{
+  window.open("https://stackoverflow.com/search?q=" + document.getElementById("llll").value )
+}
+document.getElementById('mbut').addEventListener('click', searchst);
